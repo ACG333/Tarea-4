0e475a1f32987d12df8022a0ced36fbc1004932a
ACG333 <44452066+ACG333@users.noreply.github.com>
2018-11-18 14:52:24 -0500

    Create pde.cpp

---
+++ pde.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<math.h>
+#include<fstream>
+#define max 10000
+using namespace std;
+double x;
+double y;
+double xf;
+double yf;
+double Vx;
+double Vy;
+double Vxf;
+double Vyf;
+double ax;
+double ay;
+double axf;
+double ayf;
+int tiempo=10;
+double pi=3.141592;
+double dt=0.0003;
+int main(){
+return 0;
+}
