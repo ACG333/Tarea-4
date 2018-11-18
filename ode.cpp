31ae0c1bf0faad88d17a330145f5db793c13736f
ACG333 <44452066+ACG333@users.noreply.github.com>
2018-11-18 11:08:46 -0500

    Create ode.cpp

---
+++ ode.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<math.h>
+#include<fstream>
+#define max 10000
+using namespace std;
+double x;
+double y;
+double xf;
+double yf;
+double Vx;
+double Vy;
+double Vxf;
+double Vyf;
+double ax;
+double ay;
+double axf;
+double ayf;
+int tiempo=10;
+double pi=3.141592;
+double dt=0.0003;
+void actualizar(){
+    xf=x;
+    yf=y;
+    Vxf=Vx;
+    Vyf=Vy;
+    axf=ax;
+    ayf=ay;
+}
+
+int main(){
+	return 0;
+
+}
